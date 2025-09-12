0354924d4e2bfcb140d1debcbc32977e2af0ded2
omotolani <olatundeomtolani23@gmail.com>
2025-09-12 19:17:41 +0100

    feat: pub sub v2

--- runner/subscriber.go
+++ runner/subscriber.go
@@ -5,14 +5,21 @@ import (
 	"context"
 
 	"encore.app/controller"
+	"encore.dev/pubsub"
 	"github.com/charmbracelet/log"
+)
+
+var _ = pubsub.NewSubscription(
+	controller.RunDispatchTopic,
+	"run-dispatch",
+	pubsub.SubscriptionConfig[controller.RunMessage]{
+		Handler: HandleRun,
+	},
 )
 
 // HandleRun subscribes to run dispatches. Multiple runner instances can use the
 // same consumer group for load-balanced consumption in heterogeneous environments.
-//
-//encore:subscribe topic=controller.RunDispatchTopic
-func HandleRun(ctx context.Context, msg *controller.RunMessage) error {
+func HandleRun(ctx context.Context, msg controller.RunMessage) error {
 	// TODO: Integrate monocrond + sandbox exec here.
 	// For now, just acknowledge by returning nil.
 	log.Info(msg)
